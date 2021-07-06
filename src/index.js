849bdf114825dc88d1ef77d096c0aca0625949b1
Siddhesh <siddhesh.shirodkar@sjinnovation.com>
2021-07-06 16:18:41 +0530

    added props and passed the values

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-    </div>
-  );
+    return (
+        <div className="ui container comments">
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Nice blog post"
+                avatar={faker.image.image()}
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00AM"
+                content="I like the subject"
+                avatar={faker.image.image()}
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Yesterday at 5:00PM"
+                content="I like the writing"
+                avatar={faker.image.image()}
+            />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
