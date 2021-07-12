08f1a49a867a5fad826aa4303377870d6ff3be66
Siddhesh <siddhesh.shirodkar@sjinnovation.com>
2021-07-12 16:44:12 +0530

    conditional rendering conventions for classes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render!!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -24,8 +23,12 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept location request" />;
-
     }
+
+    render() {
+        return <div className="border red">{this.renderContent()}</div>;
+    }
+
 }
 
 
