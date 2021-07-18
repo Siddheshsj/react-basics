fb7e8ef9cbeb46ec8b23f7b879c6bcc5f2d2043a
Siddhesh <siddhesh.shirodkar@sjinnovation.com>
2021-07-18 19:18:04 +0530

    handle event listeners by removing it if ref(DOM element) is NULL

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,13 +5,25 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
+
+  useEffect(() => {
     document.body.addEventListener(
       "click",
-      (event) => {
-        if (ref.current.contains(event.target)) {
-          return;
-        }
-
+      () => {
         setOpen(false);
       },
       { capture: true }
